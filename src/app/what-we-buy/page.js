0011ee9400a46187b7d71b9ace703d4cf4fa88f3
ryoong <52183545+reign021b@users.noreply.github.com>
2024-08-06 14:00:14 +0800

    updated supabase key hidden from client

--- src/app/what-we-buy/page.js
+++ src/app/what-we-buy/page.js
@@ -2,16 +2,7 @@
 
 import React, { useEffect, useRef, useState } from "react";
 import PriceItem from "./components/price-item";
-
-// supabase
-import { createClient } from "@supabase/supabase-js";
-
-// supabase
-// Create a single supabase client for interacting with your database
-const supabase = createClient(
-  "https://alfljqjdwlomzepvepun.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFsZmxqcWpkd2xvbXplcHZlcHVuIiwicm9sZSI6ImFub24iLCJpYXQiOjE2Njc4MzA3MTEsImV4cCI6MTk4MzQwNjcxMX0.PQsUIVHXlt0P2lQ7ZGvYyHQioWZ-lz9uHKLdwVX-YYY"
-);
+import supabase from "/utils/supabase/client";
 
 export default function WhatWeBuy() {
   const [items, setItems] = useState([]);
@@ -30,24 +21,21 @@ export default function WhatWeBuy() {
   async function getItems() {
     const { data } = await supabase.from("prices").select();
     console.log(data);
-    // const plasticItems = data.filter((item) => item.type == "plastic");
-    // console.log(plasticItems);
-
     setItems(data);
   }
 
   function scrollToRef(materialType) {
-    if (materialType == 1) {
+    if (materialType === 1) {
       plasticRef.current?.scrollIntoView({ behavior: "smooth" });
-    } else if (materialType == 2) {
+    } else if (materialType === 2) {
       metalRef.current?.scrollIntoView({ behavior: "smooth" });
-    } else if (materialType == 3) {
+    } else if (materialType === 3) {
       paperRef.current?.scrollIntoView({ behavior: "smooth" });
-    } else if (materialType == 4) {
+    } else if (materialType === 4) {
       ewasteRef.current?.scrollIntoView({ behavior: "smooth" });
-    } else if (materialType == 5) {
+    } else if (materialType === 5) {
       batteryRef.current?.scrollIntoView({ behavior: "smooth" });
-    } else if (materialType == 6) {
+    } else if (materialType === 6) {
       glassRef.current?.scrollIntoView({ behavior: "smooth" });
     }
   }
@@ -104,9 +92,16 @@ export default function WhatWeBuy() {
       {/* plastics */}
       <div ref={plasticRef} className="scale-90 md:scale-100 px-5 md:pt-24">
         <div className="flex">
-          <h2 className="text-2xl md:text-4xl font-medium mr-10 w-52">Plastics</h2>
+          <h2 className="text-2xl md:text-4xl font-medium mr-10 w-52">
+            Plastics
+          </h2>
           <p>
-          Plastic is a man-made material that can be shaped into many different forms. It's created by combining simple chemicals into long chains. This makes plastic flexible and strong. While plastics are durable and inexpensive, their disposal and environmental impact are significant concerns. Recycling plastics helps reduce waste and conserve resources.
+            Plastic is a man-made material that can be shaped into many
+            different forms. It's created by combining simple chemicals into
+            long chains. This makes plastic flexible and strong. While plastics
+            are durable and inexpensive, their disposal and environmental impact
+            are significant concerns. Recycling plastics helps reduce waste and
+            conserve resources.
             <br />
             <br />
             Note: Pricing for bulk scraps is higher (contact us for more info).
@@ -116,9 +111,9 @@ export default function WhatWeBuy() {
         <div className="grid grid-cols-6 gap-x-7 gap-y-10 row-auto w-full mt-10">
           {items
             .filter((item) => item.type === "plastic")
-            .map((item, index) => (
+            .map((item) => (
               <PriceItem
-                key={item.id} // Assuming 'id' is a unique identifier for each item
+                key={item.id}
                 imgUrl={item.image}
                 name={item.itemName}
                 price={item.priceRange}
@@ -130,9 +125,14 @@ export default function WhatWeBuy() {
       {/* metal */}
       <div ref={metalRef} className="scale-90 md:scale-100 px-5 md:pt-28">
         <div className="flex">
-          <h2 className="text-2xl md:text-4xl font-medium mr-10 w-52">Metals</h2>
+          <h2 className="text-2xl md:text-4xl font-medium mr-10 w-52">
+            Metals
+          </h2>
           <p>
-          Metals are materials that are usually hard, shiny, and can conduct heat and electricity. Recycling metals helps save resources and reduces waste. Instead of throwing away old metal items, recycling them allows us to reuse the metal to make new products. 
+            Metals are materials that are usually hard, shiny, and can conduct
+            heat and electricity. Recycling metals helps save resources and
+            reduces waste. Instead of throwing away old metal items, recycling
+            them allows us to reuse the metal to make new products.
             <br />
             <br />
             Note: Pricing for bulk scraps is higher (contact us for more info).
@@ -144,7 +144,7 @@ export default function WhatWeBuy() {
             .filter((item) => item.type === "metal")
             .map((item) => (
               <PriceItem
-                key={item.id} 
+                key={item.id}
                 imgUrl={item.image}
                 name={item.itemName}
                 price={item.priceRange}
@@ -158,14 +158,19 @@ export default function WhatWeBuy() {
         <div className="flex">
           <h2 className="text-2xl md:text-4xl font-medium mr-10 w-52">Paper</h2>
           <p>
-          Paper is a material made from plant fibers, usually from wood but sometimes from other plants like cotton. It’s used for things like books, newspapers, and packaging. During recycling, paper is broken down, cleaned, and reprocessed into new paper products, helping to reduce the need for virgin materials and decrease environmental impact.
+            Paper is a material made from plant fibers, usually from wood but
+            sometimes from other plants like cotton. It’s used for things like
+            books, newspapers, and packaging. During recycling, paper is broken
+            down, cleaned, and reprocessed into new paper products, helping to
+            reduce the need for virgin materials and decrease environmental
+            impact.
             <br />
             <br />
             Note: Pricing for bulk scraps is higher (contact us for more info).
           </p>
         </div>
         {/* grid */}
-        <div className="grid grid-cols-6 gap-x-7 gap-y-10 row-auto w-full mt-10">
+        <div className="grid grid-cols-6 gap-x-7 gap-y-10 row-auto w full mt-10">
           {items
             .filter((item) => item.type === "paper")
             .map((item) => (
@@ -182,21 +187,28 @@ export default function WhatWeBuy() {
       {/* e-waste */}
       <div ref={ewasteRef} className="scale-90 md:scale-100 px-5 md:pt-28">
         <div className="flex">
-          <h2 className="text-2xl md:text-4xl font-medium mr-10 w-52">E-Waste</h2>
+          <h2 className="text-2xl md:text-4xl font-medium mr-10 w-52">
+            E-Waste
+          </h2>
           <p>
-          E-waste, or electronic waste, refers to discarded electrical or electronic devices. E-waste often contains hazardous materials like lead, mercury, and cadmium, which can pose environmental and health risks if not properly managed. Recycling e-waste helps recover valuable materials and reduces the negative impact on the environment.
+            E-waste, or electronic waste, refers to discarded electrical or
+            electronic devices. E-waste often contains hazardous materials like
+            lead, mercury, and cadmium, which can pose environmental and health
+            risks if not properly managed. Recycling e-waste helps recover
+            valuable materials and reduces the negative impact on the
+            environment.
             <br />
             <br />
             Note: Pricing for bulk scraps is higher (contact us for more info).
           </p>
         </div>
         {/* grid */}
-        <div className="grid grid-cols-6 gap-x-7 gap-y-10 row-auto w-full mt-10">
+        <div className="grid grid-cols-6 gap-x-7 gap-y-10 row-auto w full mt-10">
           {items
             .filter((item) => item.type === "e-waste")
             .map((item) => (
               <PriceItem
-                key={item.id}  
+                key={item.id}
                 imgUrl={item.image}
                 name={item.itemName}
                 price={item.priceRange}
@@ -208,21 +220,26 @@ export default function WhatWeBuy() {
       {/* battery */}
       <div ref={batteryRef} className="scale-90 md:scale-100 px-5 md:pt-28">
         <div className="flex">
-          <h2 className="text-2xl md:text-4xl font-medium mr-10 w-52">Batteries</h2>
+          <h2 className="text-2xl md:text-4xl font-medium mr-10 w-52">
+            Batteries
+          </h2>
           <p>
-          Car and Motorcycle batteries are specialized batteries designed to power vehicles. Both types of batteries contain hazardous materials like lead and sulfuric acid, making proper disposal and recycling crucial for environmental safety and resource recovery.
+            Car and Motorcycle batteries are specialized batteries designed to
+            power vehicles. Both types of batteries contain hazardous materials
+            like lead and sulfuric acid, making proper disposal and recycling
+            crucial for environmental safety and resource recovery.
             <br />
             <br />
             Note: Pricing for bulk scraps is higher (contact us for more info).
           </p>
         </div>
         {/* grid */}
-        <div className="grid grid-cols-6 gap-x-7 gap-y-10 row-auto w-full mt-10">
+        <div className="grid grid-cols-6 gap-x-7 gap-y-10 row-auto w full mt-10">
           {items
             .filter((item) => item.type === "battery")
             .map((item) => (
               <PriceItem
-                key={item.id}  
+                key={item.id}
                 imgUrl={item.image}
                 name={item.itemName}
                 price={item.priceRange}
@@ -236,14 +253,20 @@ export default function WhatWeBuy() {
         <div className="flex">
           <h2 className="text-2xl md:text-4xl font-medium mr-10 w-52">Glass</h2>
           <p>
-          Glass is a hard and breakable material often used for things like windows, bottles, and jars. It’s made by melting sand and then quickly cooling it to create a solid. Glass is recyclable and can be processed repeatedly without losing quality, making it an environmentally friendly option when properly managed. Recycling glass helps conserve resources and reduces the need for raw materials.
+            Glass is a hard and breakable material often used for things like
+            windows, bottles, and jars. It’s made by melting sand and then
+            quickly cooling it to create a solid. Glass is recyclable and can be
+            processed repeatedly without losing quality, making it an
+            environmentally friendly option when properly managed. Recycling
+            glass helps conserve resources and reduces the need for raw
+            materials.
             <br />
             <br />
             Note: Pricing for bulk scraps is higher (contact us for more info).
           </p>
         </div>
         {/* grid */}
-        <div className="grid grid-cols-6 gap-x-7 gap-y-10 row-auto w-full mt-10">
+        <div className="grid grid-cols-6 gap-x-7 gap-y-10 row-auto w full mt-10">
           {items
             .filter((item) => item.type === "glass")
             .map((item) => (
